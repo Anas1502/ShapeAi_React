972153e7f9c9ff653c04e3c5b9240bf4f03f358b
Abu Anas <83357340+Anas1502@users.noreply.github.com>
2021-06-06 01:02:35 +0530

    Add files via upload

---
+++ ShapeAI_Pro/Reactjs-Starter-Repo-main/src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Note() {
+	return (
+			<div classname="note">
+				<h1> Javascript and React.js</h1>
+				<p>This was an amazing bootcamp taken up by Shaurya sinha Sir.
+				We covered everything from scratch including Javascript, React.js
+				,Html.
+				</p>
+			</div>
+	);
+}
+
+export default Note;
